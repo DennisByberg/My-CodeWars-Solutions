41bd7eef54126c69909a981dfba5fbb1f55d70c8
Dennis Byberg <dennis.byberg@hotmail.com>
2024-05-10 00:38:25 +0200

    returnTheNextNumberfromTheIntegerPassed solved + tests

---
+++ Solutions/Return_The_Next_Number_From_The_Integer_Passed/returnTheNextNumberfromTheIntegerPassed.test.ts
@@ -0,0 +1,12 @@
+import { describe, expect, it } from "vitest";
+import { addition } from "./returnTheNextNumberfromTheIntegerPassed";
+
+describe(addition, () => {
+  it("should return the next number from the integer passed", () => {
+    expect(addition(2)).toBe(3);
+    expect(addition(-9)).toBe(-8);
+    expect(addition(0)).toBe(1);
+    expect(addition(999)).toBe(1000);
+    expect(addition(73)).toBe(74);
+  });
+});
---
+++ Solutions/Return_The_Next_Number_From_The_Integer_Passed/returnTheNextNumberfromTheIntegerPassed.ts
@@ -0,0 +1,7 @@
+// EN: Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+
+// SV: Skapa en funktion som tar ett nummer som argument, ökar numret med +1 och returnerar resultatet.
+
+export function addition(num: number): number {
+  return num + 1;
+}
