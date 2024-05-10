2bfc6b7bf4c4d143d8e4dac83ad5c9cd0b55afa6
Dennis Byberg <dennis.byberg@hotmail.com>
2024-05-10 13:18:26 +0200

    buggyCodePart1 solved + tests

---
+++ Solutions/Buggy_Code_Part_1/buggyCodePart1.test.ts
@@ -0,0 +1,11 @@
+import { describe, expect, it } from "vitest";
+import { cubes } from "./buggyCodePart1";
+
+describe(cubes, () => {
+  it("should return the cubes of the given number", () => {
+    expect(cubes(3)).toEqual(27);
+    expect(cubes(5)).toEqual(125);
+    expect(cubes(10)).toEqual(1000);
+  });
+});
+export { cubes };
