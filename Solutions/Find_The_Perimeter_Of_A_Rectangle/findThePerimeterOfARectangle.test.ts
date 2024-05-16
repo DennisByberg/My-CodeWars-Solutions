fe27891edc4ead8877d68380b409c7024c5f4a2e
Dennis Byberg <dennis.byberg@hotmail.com>
2024-05-16 14:40:29 +0200

    Find The Perimeter Of A Rectangle solved + tests

---
+++ Solutions/Find_The_Perimeter_Of_A_Rectangle/findThePerimeterOfARectangle.test.ts
@@ -0,0 +1,10 @@
+import { describe, expect, it } from "vitest";
+import { findPerimeter } from "./findThePerimeterOfARectangle";
+
+describe(findPerimeter, () => {
+  it("should return the perimeter of a rectangle", () => {
+    expect(findPerimeter(6, 7)).toEqual(26);
+    expect(findPerimeter(2, 9)).toEqual(22);
+    expect(findPerimeter(20, 10)).toEqual(60);
+  });
+});
