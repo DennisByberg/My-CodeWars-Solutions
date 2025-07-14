03bc9b9461c91732d42abbf2596a72d0917cf14e
dennisbyberg <dennis.byberg@hotmail.com>
2025-07-14 13:54:44 +0200

    implement warnTheSheep function with unit tests

--- Solutions/A_Wolf_In_Sheeps_Clothing/TS/warnTheSheep.test.ts
+++ Solutions/A_Wolf_In_Sheeps_Clothing/TS/warnTheSheep.test.ts
@@ -2,10 +2,13 @@ import { describe, expect, it } from 'vitest';
 import { warnTheSheep } from './warnTheSheep';
 
 describe(warnTheSheep, () => {
-  it('Should warn the closest sheep', () => {
+  it('Should warn when wolf is at the end', () => {
     expect(warnTheSheep(['sheep', 'sheep', 'wolf'])).toBe(
       'Pls go away and stop eating my sheep'
     );
+  });
+
+  it('Should warn the closest sheep when wolf is in the middle', () => {
     expect(warnTheSheep(['sheep', 'sheep', 'sheep', 'wolf', 'sheep'])).toBe(
       'Oi! Sheep number 1! You are about to be eaten by a wolf!'
     );
