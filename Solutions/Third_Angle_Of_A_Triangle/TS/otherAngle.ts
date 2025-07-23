5d1a3e30dc9ca5dfb168d130031add71bcdf53c4
dennisbyberg <dennis.byberg@hotmail.com>
2025-07-23 13:41:32 +0200

    Refactor: TS solution

--- Solutions/Third_Angle_Of_A_Triangle/TS/otherAngle.ts
+++ Solutions/Third_Angle_Of_A_Triangle/TS/otherAngle.ts
@@ -1,5 +1,1 @@
-export const otherAngle = (a: number, b: number): number => {
-  const TOTAL_DEGREES_IN_TRIANGLE = 180;
-
-  return TOTAL_DEGREES_IN_TRIANGLE - a - b;
-};
+export const otherAngle = (a: number, b: number): number => 180 - a - b;
